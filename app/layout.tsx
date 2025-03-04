86a923a8d27293e8672b6b4820c5f5ea8689a1f9
Ranggacaw <ranggacahyaw@gmail.com>
2025-03-04 21:49:21 +0700

    change favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
     return (
         <html lang="en">
             <head>
-                <link rel="icon" href="/favicon.ico" sizes="any" />
+                <link rel="icon" href="/images/shopedia-logo.png" sizes="any" />
             </head>
             <body
                 className={`${geistSans.variable} ${geistMono.variable} antialiased`}
