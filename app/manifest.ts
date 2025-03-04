f2e82d57cd0b3d0ca03e180b8af30f449dee846d
Ranggacaw <ranggacahyaw@gmail.com>
2025-03-04 23:06:25 +0700

    add manifest.ts for pwa

---
+++ app/manifest.ts
@@ -0,0 +1,25 @@
+import type { MetadataRoute } from 'next'
+ 
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: 'Shopedia',
+    short_name: 'Shopedia',
+    description: 'You will get any fresh thing here.',
+    start_url: '/',
+    display: 'standalone',
+    background_color: '#ffffff',
+    theme_color: '#000000',
+    icons: [
+      {
+        src: '/images/shopedia-logo.png',
+        sizes: '192x192',
+        type: 'image/png',
+      },
+      {
+        src: '/images/shopedia-logo.png',
+        sizes: '512x512',
+        type: 'image/png',
+      },
+    ],
+  }
+}
